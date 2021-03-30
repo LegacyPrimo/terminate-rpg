29cc5bd36745e637a135a1b3db4ea8dca9d5614a
LegacyPrimo <johntimothy.manal@gmail.com>
2021-03-30 17:21:04 +0800

    Fading Transition Effects Added

--- Terminate RPG/Assets/Scripts/SceneTransition.cs
+++ Terminate RPG/Assets/Scripts/SceneTransition.cs
@@ -8,13 +8,41 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue playerSetPosition;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWaitingTime;
+
+    private void Awake()
+    {
+        if (fadeInPanel != null) 
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !collision.isTrigger) 
         {
+
             playerSetPosition.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(FadeCoroutine());          
+        }
+    }
+    public IEnumerator FadeCoroutine() 
+    {
+        if (fadeOutPanel != null) 
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+
+        yield return new WaitForSeconds(fadeWaitingTime);
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneToLoad);
+
+        while (!async.isDone) 
+        {
+            yield return null;
         }
     }
 }
