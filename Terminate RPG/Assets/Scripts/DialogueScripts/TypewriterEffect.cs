9a0b56af79447f3e82ca30c5b4c33fa249f3c54e
LegacyPrimo <63246748+LegacyPrimo@users.noreply.github.com>
2021-05-05 00:26:01 +0800

    Added a Typewriting Soundtrack on Opening Message

--- Terminate RPG/Assets/Scripts/DialogueScripts/TypewriterEffect.cs
+++ Terminate RPG/Assets/Scripts/DialogueScripts/TypewriterEffect.cs
@@ -9,20 +9,25 @@ public class TypewriterEffect : MonoBehaviour
     [TextArea(5,10)]
     public string fullText;
     private string currentText = "";
+    public AudioSource typeWriterSound;
 
     // Start is called before the first frame update
     void Start()
     {
+        typeWriterSound = GetComponent<AudioSource>();
         StartCoroutine(ShowTextSpeed());
     }
 
     IEnumerator ShowTextSpeed() 
     {
+        typeWriterSound.Play();
         for (int i = 0; i < fullText.Length; i++)
         {
+            
             currentText = fullText.Substring(0,i);
             this.GetComponent<Text>().text = currentText;
             yield return new WaitForSeconds(textSpeed);
         }
+        typeWriterSound.Stop();
     }
 }
