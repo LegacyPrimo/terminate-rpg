a69fe33e2bfdf1a18221aaf27e7846330a27871c
LegacyPrimo <63246748+LegacyPrimo@users.noreply.github.com>
2021-05-07 13:11:05 +0800

    South Cave Floors Implemented

--- Terminate RPG/Assets/SuperTiled2Unity/Import/GalletCityTileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="GalletCityTileset" tilewidth="16" tileheight="16" tilecount="40" columns="4">
- <image source="../../Art/Maps/City Tileset/galletcity_tiles.png" width="64" height="168"/>
-</tileset>
